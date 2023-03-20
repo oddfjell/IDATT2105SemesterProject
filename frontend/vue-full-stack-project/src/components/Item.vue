0690ad2da340f6b65baf30d8a0ab3bac8e785ce0
EmilOrv <100238981+Emilorv@users.noreply.github.com>
2023-03-20 15:21:41 +0100

    Added multiple items to the frontpage

--- frontend/vue-full-stack-project/src/components/Item.vue
+++ frontend/vue-full-stack-project/src/components/Item.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="container">
-  <img alt="bird" src="https://ctl.s6img.com/society6/img/T4B1_3uYoossKHshTuH-AWt3I2I/w_1500/prints/~artwork/s6-original-art-uploads/society6/uploads/misc/dd1abcfa933c48e982c730c3cc154cf2/~~/yellow-red-lovebird-lovebird-bird-pets-cute-cute-bird-yellow-lovebird-yellow-bird-prints.jpg">
+    <div id="thumbnail">
+      <img alt="bird" src="https://ctl.s6img.com/society6/img/T4B1_3uYoossKHshTuH-AWt3I2I/w_1500/prints/~artwork/s6-original-art-uploads/society6/uploads/misc/dd1abcfa933c48e982c730c3cc154cf2/~~/yellow-red-lovebird-lovebird-bird-pets-cute-cute-bird-yellow-lovebird-yellow-bird-prints.jpg">
+    </div>
   <label>En fugl</label>
   <p>Pris: 500kr</p>
   <p>Dette er en beskrivelse</p>
@@ -25,5 +27,9 @@ export default {
   padding: 10px;
   border-radius: 5px;
   border: solid black 4px;
+}
+#thumbnail{
+  width: 80%;
+  align-content: center;
 }
 </style>
--- frontend/vue-full-stack-project/src/views/FrontPageView.vue
+++ frontend/vue-full-stack-project/src/views/FrontPageView.vue
@@ -1,7 +1,11 @@
 <template>
   <main>
     <h1>ELO</h1>
+    <div id="item_grid">
+    <div id="item" :key="item" v-for="item in items">
     <ItemComponent/>
+    </div>
+    </div>
   </main>
 </template>
 
@@ -10,10 +14,26 @@ import ItemComponent from "@/components/Item.vue";
 
 export default {
   name: "FrontPageView.vue",
-  components: {ItemComponent}
+  components: {ItemComponent},
+  data(){
+    return{
+      props:{
+        items:Array
+      }
+    }
+  },
+  created() {
+    this.items=[1,2,3,4,5]
+  }
 }
+
 </script>
 
 <style scoped>
+#item_grid{
+  display: grid;
+  grid-template-columns: fit-content(5%);
+  grid-gap: 5%;
+}
 
 </style>
