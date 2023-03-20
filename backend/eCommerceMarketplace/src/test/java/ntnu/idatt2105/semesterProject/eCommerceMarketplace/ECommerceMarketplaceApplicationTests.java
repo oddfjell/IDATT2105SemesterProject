7a67a38f0e31827224886c5e0d189e13b8b1f15d
Ann-Marie Revillard <ann.marie.amr@gmail.com>
2023-03-20 10:02:47 +0100

    made a spring boot backend format

---
+++ backend/eCommerceMarketplace/src/test/java/ntnu/idatt2105/semesterProject/eCommerceMarketplace/ECommerceMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package ntnu.idatt2105.semesterProject.eCommerceMarketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
