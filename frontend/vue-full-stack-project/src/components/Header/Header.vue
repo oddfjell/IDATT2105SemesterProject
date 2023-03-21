49c4d5ea80e75fda68051ca2c2089b5202ba1bb1
EmilOrv <100238981+Emilorv@users.noreply.github.com>
2023-03-21 14:48:32 +0100

    Refactored the header to its own component, and made it possible to switch navbar based on loggin-status

---
+++ frontend/vue-full-stack-project/src/components/Header/Header.vue
@@ -0,0 +1,43 @@
+<template>
+<header>
+  <img id="logo" src="https://dieselpunkcore.com/wp-content/uploads/2014/06/logo-placeholder.png" alt="">
+  <h1 id="title">Tittel</h1>
+</header>
+    <default-nav-bar v-if="isLoggedIn"/>
+  <LoggedInNavBar v-else />
+</template>
+
+<script>
+import DefaultNavBar from "@/components/Header/DefaultNavBar.vue";
+import LoggedInNavBar from "@/components/Header/LoggedInNavBar.vue";
+
+export default {
+  name: "DefaultHeader.vue",
+  components: {LoggedInNavBar, DefaultNavBar},
+  data(){
+    return {
+        isLoggedIn:Boolean
+    }
+  },
+  created() {
+    this.isLoggedIn = false;
+  }
+}
+</script>
+
+<style scoped>
+
+#logo{
+  width: min(50vw, 150px);
+}
+#title{
+  display: inline;
+}
+header {
+  background: #1abc9c;
+  color: white;
+  font-size: xx-large;
+}
+
+
+</style>
---
+++ frontend/vue-full-stack-project/src/components/Header/LoggedInNavBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav>
+    <div id="routerList">
+      <router-link class="navElement" to="/">Profile</router-link>
+      <router-link class="navElement" to="/">Cart</router-link>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "LoggedInNavBar"
+}
+</script>
+
+<style scoped>
+nav{
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+#routerList{
+  margin-right: 20px;
+}
+.navElement{
+  float:right;
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 15px 20px;
+  text-decoration: none;
+}
+.navElement:hover{
+  background-color: #555555;
+}
+</style>
