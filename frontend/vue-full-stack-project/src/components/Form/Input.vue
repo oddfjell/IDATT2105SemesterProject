5e19e6c893b2470eeba52434459b30483cdeb3cb
EmilOrv <100238981+Emilorv@users.noreply.github.com>
2023-03-23 11:59:59 +0100

    Improved inputcomponent styling

--- frontend/vue-full-stack-project/src/components/Form/Input.vue
+++ frontend/vue-full-stack-project/src/components/Form/Input.vue
@@ -1,17 +1,19 @@
 <template>
+  <div>
   <label v-if="label"> {{ label }} </label>
   <input
       v-bind="$attrs"
+      :type="type"
       :value="modelValue"
       :placeholder="label"
       @input="$emit('update:modelValue', $event.target.value)"
   />
   <p id="error" v-if="error">{{ error }}</p>
+  </div>
 </template>
 
 <script>
 export default {
-  name:"Base-input",
   props: {
     label: {
       type: String,
@@ -25,11 +27,25 @@ export default {
       type: [String, Number],
       default: "",
     },
+    type:[String,Number]
   },
 };
 </script>
 
 <style scoped>
+input{
+  width: 100%;
+  margin: auto;
+  vertical-align: center;
+  padding: 3%;
+
+}
+label{
+
+  margin: auto;
+  display: block;
+  vertical-align: center;
+}
 #error {
   color: red;
   font-size: small;
