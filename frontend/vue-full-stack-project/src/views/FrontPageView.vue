f3f3ae4ba50095fba0d688fb078673fc94a684f6
EmilOrv <100238981+Emilorv@users.noreply.github.com>
2023-03-24 16:53:32 +0100

    Made frontpage view show products even when apicall fails

--- frontend/vue-full-stack-project/src/views/FrontPageView.vue
+++ frontend/vue-full-stack-project/src/views/FrontPageView.vue
@@ -22,6 +22,7 @@ export default {
   components: {ItemComponent},
   data(){
     return{
+      items:Array
     }
   },
   methods:{
@@ -30,18 +31,8 @@ export default {
       this.store.selectItem(item)
     }
   },
-  async setup() {
-    let items=[]
-    let getItems = await itemService.getItems()
-    for(let item of getItems){
-      items.unshift(item)
-    }
-    const store = Itemstore()
-    return{
-      store, items
-    }
-  },
-  created() {
+  created(){
+    console.log("Hei")
     let additionalItems=[
       {title:"Bird", price:100, briefDescription:"This is a bird", description:"This is a longer text that says bird",image:"https://ctl.s6img.com/society6/img/T4B1_3uYoossKHshTuH-AWt3I2I/w_1500/prints/~artwork/s6-original-art-uploads/society6/uploads/misc/dd1abcfa933c48e982c730c3cc154cf2/~~/yellow-red-lovebird-lovebird-bird-pets-cute-cute-bird-yellow-lovebird-yellow-bird-prints.jpg"},
       {title:"Dog", price:200, briefDescription:"This is a dog", description:"This is a longer text that says dog",image:"https://thumbs.dreamstime.com/b/happy-dog-puppy-smiling-colored-blue-backgorund-closed-eyes-happy-dog-puppy-smiling-colored-blue-backgorund-193236837.jpg"},
@@ -49,8 +40,25 @@ export default {
       {title:"Capybara", price:9999, briefDescription:"Capybara", description:"This is a longer text that says capybara",image:"https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Capybara_%28Hydrochoerus_hydrochaeris%29.JPG/1200px-Capybara_%28Hydrochoerus_hydrochaeris%29.JPG"},]
     for(let item of additionalItems){
       this.items.push(item)
+      console.log(this.items)
     }
-  }
+  },
+  async setup() {
+      let items = []
+    try {
+      let getItems = await itemService.getItems()
+      for (let item of getItems) {
+        items.unshift(item)
+      }
+    }catch (e){
+      console.log(e)
+    }
+    const store = Itemstore()
+    return{
+      store, items
+    }
+  },
+
 }
 
 </script>
