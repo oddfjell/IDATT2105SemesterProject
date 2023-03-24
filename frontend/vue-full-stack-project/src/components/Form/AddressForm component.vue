5a0a3bd58b468fb8eb0e2b18d45ab0990300cebe
EmilOrv <100238981+Emilorv@users.noreply.github.com>
2023-03-24 16:53:54 +0100

    Added potensial addressComponent

---
+++ frontend/vue-full-stack-project/src/components/Form/AddressForm component.vue
@@ -0,0 +1,24 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "AddressForm-component",
+  data() {
+    return {
+      address: {
+        country: String,
+        city: String,
+        postal_code: Number,
+        street_name: String,
+        street_number: Number,
+      }
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
