57c561d4b5d5c8ad996fc60fb4969033b27509ac
EmilOrv <emilols.eo@gmail.com>
2023-03-25 17:20:19 +0100

    Changes header-navbar based on log-in status

--- frontend/vue-full-stack-project/src/components/Header/Header.vue
+++ frontend/vue-full-stack-project/src/components/Header/Header.vue
@@ -10,6 +10,7 @@
 <script>
 import DefaultNavBar from "@/components/Header/DefaultNavBar.vue";
 import LoggedInNavBar from "@/components/Header/LoggedInNavBar.vue";
+import {useTokenStore} from "@/stores/token";
 
 export default {
   name: "DefaultHeader.vue",
@@ -20,8 +21,15 @@ export default {
     }
   },
   created() {
-    this.isLoggedIn = false;
-  }
+    this.isLoggedIn=false;
+    if( this.tokenStore.loggedInUser !=null){
+      this.isLoggedIn=true;
+    }
+  },
+  setup() {
+    const tokenStore = useTokenStore();
+    return { tokenStore };
+  },
 }
 </script>
 
