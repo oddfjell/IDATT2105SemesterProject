d4acf6755588abe095733f6e815d1e5b17ab7e9a
EmilOrv <100238981+Emilorv@users.noreply.github.com>
2023-03-27 13:12:03 +0200

    Removed "Searchbar"

--- frontend/vue-full-stack-project/src/components/ItemGrid.vue
+++ frontend/vue-full-stack-project/src/components/ItemGrid.vue
@@ -1,12 +1,12 @@
 
 <template>
   <main>
-    <h1>Her kommer det evt. en søkebar</h1>
-    <div id="item_grid">
+    <div v-if="items" id="item_grid">
       <div class="item hoverborder" :key="item.title" v-for="(item,index) in items">
         <ItemComponent :tabindex="index+1" :item="item" v-on:keyup.enter="selectItem(item)" @click="selectItem(item)"/>
       </div>
     </div>
+    <div v-else><h1>No items :(</h1></div>
   </main>
 </template>
 
@@ -34,10 +34,10 @@ export default {
   },
   created(){
     let additionalItems=[
-      {title:"Bird", price:100, briefDescription:"This is a bird", description:"This is a longer text that says bird",image:"https://ctl.s6img.com/society6/img/T4B1_3uYoossKHshTuH-AWt3I2I/w_1500/prints/~artwork/s6-original-art-uploads/society6/uploads/misc/dd1abcfa933c48e982c730c3cc154cf2/~~/yellow-red-lovebird-lovebird-bird-pets-cute-cute-bird-yellow-lovebird-yellow-bird-prints.jpg"},
-      {title:"Dog", price:200, briefDescription:"This is a dog", description:"This is a longer text that says dog",image:"https://thumbs.dreamstime.com/b/happy-dog-puppy-smiling-colored-blue-backgorund-closed-eyes-happy-dog-puppy-smiling-colored-blue-backgorund-193236837.jpg"},
-      {title:"Cat", price:1000, briefDescription:"This is a cat", description:"This is a longer text that says cat",image:"https://i.ytimg.com/vi/YSHDBB6id4A/maxresdefault.jpg"},
-      {title:"Capybara", price:9999, briefDescription:"Capybara", description:"This is a longer text that says capybara",image:"https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Capybara_%28Hydrochoerus_hydrochaeris%29.JPG/1200px-Capybara_%28Hydrochoerus_hydrochaeris%29.JPG"},]
+      {title:"Bird", price:100, briefDescription:"This is a bird", description:"This is a longer text that says bird",image:"https://ctl.s6img.com/society6/img/T4B1_3uYoossKHshTuH-AWt3I2I/w_1500/prints/~artwork/s6-original-art-uploads/society6/uploads/misc/dd1abcfa933c48e982c730c3cc154cf2/~~/yellow-red-lovebird-lovebird-bird-pets-cute-cute-bird-yellow-lovebird-yellow-bird-prints.jpg",user:0},
+      {title:"Dog", price:200, briefDescription:"This is a dog", description:"This is a longer text that says dog",image:"https://thumbs.dreamstime.com/b/happy-dog-puppy-smiling-colored-blue-backgorund-closed-eyes-happy-dog-puppy-smiling-colored-blue-backgorund-193236837.jpg",user:0},
+      {title:"Cat", price:1000, briefDescription:"This is a cat", description:"This is a longer text that says cat",image:"https://i.ytimg.com/vi/YSHDBB6id4A/maxresdefault.jpg", user:0},
+      {title:"Capybara", price:9999, briefDescription:"Capybara", description:"This is a longer text that says capybara",image:"https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Capybara_%28Hydrochoerus_hydrochaeris%29.JPG/1200px-Capybara_%28Hydrochoerus_hydrochaeris%29.JPG", user:0},]
     for(let item of additionalItems){
       this.items.push(item)
     }
@@ -69,6 +69,7 @@ export default {
 
   grid-template-columns: 1fr 1fr 1fr 1fr;
   grid-gap: 2%;
+  margin-top: 30px;
   margin-left: 5%;
   margin-right: 5%;
 }
