f4e694f5c8bacb1ef582d3a46128d0f5478fa1c1
Jonas Mørkland <jonasmorkland@outlook.com>
2023-03-27 14:26:46 +0200

    Pushed up so Emil can continue at frontend

--- frontend/vue-full-stack-project/src/views/RegisterPageView.vue
+++ frontend/vue-full-stack-project/src/views/RegisterPageView.vue
@@ -1,9 +1,10 @@
 <template>
-  <Header />
+  <Header/>
   <back-header/>
   <div class="container">
     <h1>Register</h1>
     <form id="loginField" class="loginField" @submit.prevent="submit">
+
       <BaseInput
           v-model="user.username"
           :modelValue="user.username"
@@ -13,6 +14,7 @@
           type="text"
           class="field"
       />
+
       <BaseInput
           v-model="user.password"
           :modelValue="user.password"
@@ -22,6 +24,7 @@
           type="password"
           class="field"
       />
+
       <div id="name" class="field">
         <BaseInput
             v-model="user.firstName"
@@ -31,6 +34,7 @@
             label="Firstname"
             type="text"
         />
+
         <BaseInput
             v-model="user.lastName"
             :modelValue="user.lastName"
@@ -39,16 +43,19 @@
             label="Lastname"
             type="text"
         />
+
       </div>
+
       <BaseInput
-      v-model="user.email"
-      :modelValue="user.email"
-      :error="errors.email"
-      @input="onInputEmail"
-      label="Email"
-      type="email"
-      class="field"
+          v-model="user.email"
+          :modelValue="user.email"
+          :error="errors.email"
+          @input="onInputEmail"
+          label="Email"
+          type="email"
+          class="field"
       />
+
       <BaseInput
           v-model="user.phoneNumber"
           :modelValue="user.phoneNumber"
@@ -59,7 +66,7 @@
           class="field"
       />
 
-     <BaseInput
+      <BaseInput
           v-model="user.dateOfBirth"
           :modelValue="dateOfBirth"
           :error="errors.dateOfBirth"
@@ -68,13 +75,64 @@
           type="date"
           class="field"
       />
-      <ImagePicker label="Profile picture" class="field" />
+
+      <BaseInput
+          v-model="user.address.city"
+          :modelValue="user.address.city"
+          :error="errors.city"
+          @input="onInputCity"
+          label="City"
+          type="text"
+          class="field"
+      />
+
+      <BaseInput
+          v-model="user.address.country"
+          :modelValue="user.address.country"
+          :error="errors.country"
+          @input="onInputCountry"
+          label="Country"
+          type="text"
+          class="field"
+      />
+
+      <BaseInput
+          v-model="user.address.postalCode"
+          :modelValue="user.address.postalCode"
+          :error="errors.postalCode"
+          @input="onInputPostalCode"
+          label="Postal code"
+          type="number"
+          class="field"
+      />
+
+      <BaseInput
+          v-model="user.address.streetName"
+          :modelValue="user.address.streetName"
+          :error="errors.streetName"
+          @input="onInputStreetName"
+          label="Street name"
+          type="text"
+          class="field"
+      />
+
+      <BaseInput
+          v-model="user.address.streetNumber"
+          :modelValue="user.address.streetNumber"
+          :error="errors.streetNumber"
+          @input="onInputStreetNumber"
+          label="Street number"
+          type="text"
+          class="field"
+      />
+
+      <ImagePicker label="Profile picture" class="field"/>
       <div class="Btn">
         <button id="registerBtn" type="submit">Register</button>
       </div>
     </form>
     <div class="Btn">
-      <button id="loginBtn" @click="onLogin" >Already have an account? Log in here!</button>
+      <button id="loginBtn" @click="onLogin">Already have an account? Log in here!</button>
     </div>
   </div>
 </template>
@@ -83,7 +141,7 @@
 //import setUserService from "@/services/setUserService";
 import userService from "@/services/userService";
 import BaseInput from "@/components/Form/Input.vue";
-import { useField, useForm } from "vee-validate";
+import {useField, useForm} from "vee-validate";
 import {string, object, date, number} from "yup";
 import router from "@/router";
 import BackHeader from "@/components/Header/backHeader.vue";
@@ -98,13 +156,20 @@ export default {
       user: {
         username: "",
         password: "",
-        firstName:"",
-        lastName:"",
+        firstName: "",
+        lastName: "",
         email: "",
-        phoneNumber:number,
-        dateOfBirth:date,
-        registered:date,
-        image:"",
+        phoneNumber: number,
+        dateOfBirth: date,
+        registered: date,
+        image: "",
+        address: {
+          city: "",
+          country: "",
+          postalCode: number,
+          streetName: "",
+          streetNumber: ""
+        }
       },
       errorText: ""
     };
@@ -120,19 +185,40 @@ export default {
     onInputPassword(password) {
       this.password = password.target.value;
     },
-    onInputFirstname(firstname){
+    onInputFirstname(firstname) {
       this.firstName = firstname.target.value
     },
-    onInputLastname(lastname){
+    onInputLastname(lastname) {
       this.lastName = lastname.target.value
     },
-    onInputPhone(phone){
+    onInputPhone(phone) {
       this.phoneNumber = phone.target.value
     },
-    onInputDateOfBirth(date){
+    onInputDateOfBirth(date) {
       this.dateOfBirth = date.target.value
     },
-    onLogin(){
+
+    onInputCity(city) {
+      this.city = city.target.value
+    },
+
+    onInputCountry(country) {
+      this.country = country.target.value
+    },
+
+    onInputPostalCode(postalCode) {
+      this.postalCode = postalCode.target.value
+    },
+
+    onInputStreetName(streetName) {
+      this.streetName = streetName.target.value
+    },
+
+    onInputStreetNumber(streetNumber) {
+      this.streetNumber = streetNumber.target.value
+    },
+
+    onLogin() {
       router.push("/login")
     }
   },
@@ -141,24 +227,43 @@ export default {
       username: string("Wrong format").required("Cannot be empty"),
       email: string("Wrong format").email("Please enter a valid email").required("Cannot be empty"),
       password: string("Wrong format").required("Cannot be empty"),
-      firstName:string("Wrong format").required("Cannot be empty"),
-      lastName:string("Wrong format").required("Cannot be empty"),
-      phoneNumber:number("Must be a number").positive("Must be positive").min(8, "Must be more than 8").required("Please enter a phone number"),
-      dateOfBirth:date("Must be a valid date")
+      firstName: string("Wrong format").required("Cannot be empty"),
+      lastName: string("Wrong format").required("Cannot be empty"),
+      phoneNumber: number("Must be a number").positive("Must be positive").min(8, "Must be more than 8").required("Please enter a phone number"),
+      dateOfBirth: date("Must be a valid date"),
+
+      address: {
+        city: string("Wrong format").required("Cannot be empty"),
+        country: string("Wrong format").required("Cannot be empty"),
+        postalCode: number("Must be a number").positive("Must be positive").min(1000, "Must be more than 999").max(9999, "Must be less than 10000").required("Please enter a phone number"),
+        streetName: string("Wrong format").required("Cannot be empty"),
+        streetNumber: string("Wrong format").required("Cannot be empty"),
+      }
+
     });
     const {handleSubmit, errors} = useForm({
       validationSchema,
     });
+
     const {value: username} = useField("username");
     const {value: email} = useField("email");
     const {value: password} = useField("password");
     const {value: firstName} = useField("firstName");
     const {value: lastName} = useField("lastName");
     const {value: phoneNumber} = useField("phoneNumber");
-    const{value:dateOfBirth} = useField("dateOfBirth")
+    const {value: dateOfBirth} = useField("dateOfBirth")
+
+
+    const {value: city} = useField("city")
+    const {value: country} = useField("country")
+    const {value: postalCode} = useField("postalCode");
+    const {value: streetName} = useField("streetName");
+    const {value: streetNumber} = useField("streetNumber");
+
     const submit = handleSubmit((values) => {
       userService.registerUser(values);
     });
+
     return {
       username,
       email,
@@ -168,29 +273,38 @@ export default {
       lastName,
       phoneNumber,
       dateOfBirth,
+      address: {
+        city,
+        country,
+        postalCode,
+        streetName,
+        streetNumber
+      },
       submit,
     };
   }
-  }
+}
 </script>
 
 <style scoped>
-.field{
+.field {
   display: block;
   width: 80%;
   margin: 10px auto;
 }
 
-.Btn{
+.Btn {
   margin: 20px auto;
   text-align: center;
 }
-#name{
+
+#name {
   display: grid;
   grid-template-columns: 1fr 1fr;
   grid-gap: 5%;
 }
-#registerBtn{
+
+#registerBtn {
   background: #0b6dff;
   border: 0;
   padding: 10px 20px;
@@ -198,7 +312,8 @@ export default {
   color: white;
   border-radius: 5px;
 }
-#loginBtn{
+
+#loginBtn {
   background: #b7b7b7;
   border: 0;
   padding: 10px 20px;
@@ -206,11 +321,13 @@ export default {
   color: black;
   border-radius: 5px;
 }
-#registerBtn:hover{
+
+#registerBtn:hover {
   background-color: #4169a8;
   cursor: pointer;
 }
-#loginBtn:hover{
+
+#loginBtn:hover {
   background-color: #d0cece;
   cursor: pointer;
 }
