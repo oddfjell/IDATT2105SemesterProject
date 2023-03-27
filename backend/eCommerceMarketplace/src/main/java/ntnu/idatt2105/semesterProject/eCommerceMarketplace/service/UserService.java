beebacdc676689c24696ad51a8ec43a9ffcaa86d
Jonas Mørkland <jonasmorkland@outlook.com>
2023-03-27 18:18:09 +0200

    Refactored backend logic.

--- backend/eCommerceMarketplace/src/main/java/ntnu/idatt2105/semesterProject/eCommerceMarketplace/service/UserService.java
+++ backend/eCommerceMarketplace/src/main/java/ntnu/idatt2105/semesterProject/eCommerceMarketplace/service/UserService.java
@@ -5,163 +5,63 @@ import ntnu.idatt2105.semesterProject.eCommerceMarketplace.model.LoginResponse;
 import ntnu.idatt2105.semesterProject.eCommerceMarketplace.repositories.UserRepository;
 import ntnu.idatt2105.semesterProject.eCommerceMarketplace.security.service.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
-
 import java.util.Map;
-import java.util.Optional;
+
 @Service
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
-
     @Autowired
     private TokenService tokenService;
 
-    /**
-     * Returns all the users
-     * @return Iterable<User>
-     */
+
+    // Logic for creating a new user with given user data
+    public int createUser(User user) {
+        int returnedValue = checkIfFieldsAreAlreadyInUse(user);
+
+        // If returnedValue is 3, neither username, phoneNumber nor email has been taken
+        if (returnedValue == 3) {
+            userRepository.save(user);
+        }
+        return returnedValue;
+    }
+
+
+    // Logic for returning all users
     public Iterable<User> getAllUsers() {
         return userRepository.findAll();
-        //return new ResponseEntity<>(userRepository.findAll(), HttpStatus.OK);
-    }
-
-    /**
-     * login
-     * @param payload Map<String, Object>
-     * @return LoginResponse
-     */
-    public LoginResponse loginUser(Map<String, Object> payload) {//boolean
-        String username = payload.get("username").toString();
-        String password = payload.get("password").toString();
-
-        for (User user : userRepository.findAll()) {
-            if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
-                String jwt = tokenService.generateToken(username);
-                int id = userRepository.findByUsername(username).getId();
-                String role = userRepository.findByUsername(username).getRole();
-                if(role == null){
-                    role = "ROLE_USER";
-                }
-                return new LoginResponse(jwt, id, role);
-            }
-        }
-        return null;
-    }
-
-    /**
-     * Creates new user and adds it to database
-     * Proposed solution for return values:
-     *  0: username ALREADY EXIST
-     *  1: phoneNumber ALREADY EXIST
-     *  2: email ALREADY EXIST
-     *  3: CREATED
-     *  4: ERROR
-     * @param user User
-     * @return int
-     */
-    public int createUser(User user) { //TODO or register??
-        try {
-            // Checks if user with given username already exist in database
-            if (userRepository.findByUsername(user.getUsername()) != null) {
-                return 0;
-            }
-
-            // Checks if user with given phone_number already exist in database
-            if (userRepository.findByPhoneNumber(user.getPhoneNumber()) != null) {
-                return 1;
-            }
-
-            // Checks if user with given phone_number already exist in database
-            if (userRepository.findByEmail(user.getEmail()) != null) {
-                return 2;
-            }
-
-            // Since username, phoneNumber and email were not already taken, user is created
-            userRepository.save(user);
-            return 3;
-
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            return 4;
-        }
     }
 
 
-    /**
-     * Get user
-     * @param username
-     * @return
-     */
+    // Logic for returning user with given username
     public User getUserByUsername(String username){
-
         return userRepository.findByUsername(username);
-        /*if(user != null){
-            return user;
-        } else return null;*/
-        //throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Access denied, wrong credentials....");
     }
 
-    /**
-     * Returns s user with given id
-     * @param id int
-     * @return Optional<User>
-     */
+
+    // Logic for returning user with given id
     public User getUserById(int id) {
         return userRepository.findById(id);
-    }//TODO username
-
-    /**
-     * Update user (id must be present in payload, or new user will be created)
-     * Proposed solution for return values:
-     *  0: username ALREADY EXIST
-     *  1: phoneNumber ALREADY EXIST
-     *  2: email ALREADY EXIST
-     *  3: CREATED
-     *  4: ERROR
-     * @param user User
-     * @return int
-     */
-    public int updateUser(User user) { //TODO sende string??
-        try {
-
-            System.out.println(user.getPhoneNumber());
-            System.out.println(userRepository.findByPhoneNumber(user.getPhoneNumber()));
-
-            // Checks if user with given username already exist in database
-            if (userRepository.findByUsername(user.getUsername()) != null) {
-                return 0;
-            }
-
-            // Checks if user with given phone_number already exist in database
-            if (userRepository.findByPhoneNumber(user.getPhoneNumber()) != null) {
-                return 1;
-            }
-
-            // Checks if user with given phone_number already exist in database
-            if (userRepository.findByEmail(user.getEmail()) != null) {
-                return 2;
-            }
-
-            // Update here
-            // userRepository.save(user);
-            return 3;
-
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            return 4;
-        }
     }
 
-    /**
-     * Delete user (id must be present in payload, or new user will be created)
-     * @param user User
-     * @return boolean
-     */
+
+    // Logic for updating a existing user with given user data (NOT FINISHED!!!)
+    public int updateUser(User user) {
+
+        int returnedValue = checkIfFieldsAreAlreadyInUse(user);
+
+        // If returnedValue is 3, neither username, phoneNumber nor email has been taken
+        if (returnedValue == 3) {
+            // UPDATE USER
+            // userRepository.save(user);
+        }
+        return returnedValue;
+    }
+
+
+    // Logic for deleting a user with given user data
     public boolean deleteUser(User user) {
         try {
 
@@ -180,11 +80,58 @@ public class UserService {
         }
     }
 
+
+    // ?
     public String isAdmin(User user){
         String role = userRepository.findByUsername(user.getUsername()).getRole();
         if (role == null){
             role = "ROLE_USER";
         }
         return role;
+    }
+
+
+    // ?
+    public LoginResponse loginUser(Map<String, Object> payload) {//boolean
+        String username = payload.get("username").toString();
+        String password = payload.get("password").toString();
+
+        for (User user : userRepository.findAll()) {
+            if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
+                String jwt = tokenService.generateToken(username);
+                int id = userRepository.findByUsername(username).getId();
+                String role = userRepository.findByUsername(username).getRole();
+                if(role == null){
+                    role = "ROLE_USER";
+                }
+                return new LoginResponse(jwt, id, role);
+            }
+        }
+        return null;
+    }
+
+    public int checkIfFieldsAreAlreadyInUse(User user) {
+        try {
+            // Checks if user with given username already exist in database
+            if (userRepository.findByUsername(user.getUsername()) != null) {
+                return 0;
+            }
+
+            // Checks if user with given phone_number already exist in database
+            if (userRepository.findByPhoneNumber(user.getPhoneNumber()) != null) {
+                return 1;
+            }
+
+            // Checks if user with given phone_number already exist in database
+            if (userRepository.findByEmail(user.getEmail()) != null) {
+                return 2;
+            }
+
+            return 3;
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return 4;
+        }
     }
 }
